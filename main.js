70d012a1810e4753c973e4a3c4d821e11aebf02b
Heath Lester <h.allen.lester@gmail.com>
2020-10-16 09:15:47 -0500

    initial commit

---
+++ main.js
@@ -0,0 +1,5 @@
+// 1. Define a function called "makeSandwich" that takes 4 parameters: protein, cheese, veggie, bread
+// 2. makeSandwich should create a new sandwich object with the 4 parameters as its key/value pairs
+// 3. makeSandwich will return our newly created sandwich
+// 4. Call it a few times passing in different arguments to make different sandwiches!
+
